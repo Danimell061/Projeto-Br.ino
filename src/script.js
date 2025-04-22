bd8a3083227ef73dbb5f89b2e58314266b67a7e8
Danimell061 <dani9ct@gmail.com>
2025-04-22 17:46:27 -0300

    Carrosel - Daniel

---
+++ src/script.js
@@ -0,0 +1,11 @@
+let index = 0;
+
+const carousel = document.getElementById("animacao");
+const slides = document.querySelectorAll(".slide");
+
+function moveSlide(direction) {
+    index += direction;
+    if (index < 0) index = slides.length - 1;
+    if (index >= slides.length) index = 0;
+    carousel.style.transform = `translateX(${-index * 100}%)`;
+}
